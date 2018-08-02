bbf07e9ef6250869e85eeac235e5e3299fb3cd42
HuangXiaoquan127 <xiaoquan.huang@qq.com>
2018-08-02 09:19:08 +0800

    Add youbot-manipulation

---
+++ youbot-manipulation/youbot_moveit/src/youbot_plan_group_state.cpp
@@ -0,0 +1,56 @@
+#include <moveit/move_group_interface/move_group.h>
+
+#include <moveit_msgs/DisplayTrajectory.h>
+
+int main(int argc, char **argv)
+{
+    // Initialize ROS, create the node handle and an async spinner
+    ros::init(argc, argv, "move_group_plan_group_state");
+    ros::NodeHandle nh;
+
+    ros::AsyncSpinner spin(1);
+    spin.start();
+
+    // Get the arm planning group
+    moveit::planning_interface::MoveGroup plan_group("arm_1"); //arm_1, arm_1_gripper
+
+    // Create a published for the arm plan visualization
+    ros::Publisher display_pub = nh.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
+
+    // Get the current RobotState, which will be used to set the arm
+    // to one of the predefined group states, in this case home
+    robot_state::RobotState current_state = *plan_group.getCurrentState();
+
+    // We set the state values for this robot state to the predefined
+    // group state values
+    current_state.setToDefaultValues(current_state.getJointModelGroup("arm_1"), "open"); //candle, folded, open, close
+
+    // We set the current state values to the target values
+    plan_group.setJointValueTarget(current_state);	
+
+	//ROS_INFO_STREAM("Target pose: " << plan_group.getPoseTarget());
+	//ROS_INFO_STREAM("JointValueTarget: " << plan_group.getJointValueTarget());
+	
+    // Perform the planning step, and if it succeeds display the current
+    // arm trajectory and move the arm
+    moveit::planning_interface::MoveGroup::Plan goal_plan;
+    if (plan_group.plan(goal_plan))
+    {
+        moveit_msgs::DisplayTrajectory display_msg;
+        display_msg.trajectory_start = goal_plan.start_state_;
+        display_msg.trajectory.push_back(goal_plan.trajectory_);
+        display_pub.publish(display_msg);		
+		//ROS_INFO_STREAM("Target pose: " << plan_group.getPoseTarget());  //errro: Pose for end-effector 'gripper_palm_link' not known.
+		//ROS_INFO_STREAM("Current pose: " << plan_group.getCurrentPose());
+		
+        sleep(5.0);
+
+        plan_group.move();
+    }
+
+    //ROS_INFO_STREAM("Target pose: " << plan_group.getPoseTarget()); 
+    ROS_INFO_STREAM("Current pose: " << plan_group.getCurrentPose());
+    ros::shutdown();
+
+    return 0;
+}
---
+++ youbot-manipulation/youbot_moveit/src/youbot_plan_single_target.cpp
@@ -0,0 +1,65 @@
+#include <moveit/move_group_interface/move_group.h>
+
+#include <moveit_msgs/DisplayTrajectory.h>
+
+int main(int argc, char **argv)
+{
+    // Initialize ROS, create the node handle and an async spinner
+    ros::init(argc, argv, "move_group_plan_single_target");
+    ros::NodeHandle nh;
+
+    ros::AsyncSpinner spin(1); //Use 1 threads, AsyncSpinner() can open more than one thread.
+    spin.start();
+
+    // Get the arm planning group
+    moveit::planning_interface::MoveGroup plan_group("arm_1");  //the name of group come from the moveit setup_assistant
+
+    // Create a published for the arm plan visualization
+    ros::Publisher display_pub = nh.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
+
+    // Set a goal message as a pose of the end effector
+    geometry_msgs::Pose goal;
+	
+	//home pose
+    goal.orientation.x = -0.00385699;
+    goal.orientation.y = 0.176664;
+    goal.orientation.z = -0.211258;
+    goal.orientation.w = 0.961325;
+    goal.position.x = 0.186526;
+    goal.position.y = -0.00384618;
+    goal.position.z = 0.371612;
+
+    // Set the tolerance to consider the goal achieved
+    plan_group.setGoalTolerance(0.2);
+	//plan_group.setPlannerId("RRTConnectkConfigDefault");
+
+    // Set the target pose, which is the goal we already defined
+    //bool isSetPoseTarget = plan_group.setPoseTarget(goal);
+	plan_group.setPoseTarget(goal);
+	//plan_group.setPositionTarget(goal.position.x, goal.position.y, goal.position.z);
+	//plan_group.setJointValueTarget(goal);  //add by hxq, using setJointValueTarget in youbot will not occur error
+
+	//ROS_INFO_STREAM("isSetPoseTarget: " << isSetPoseTarget);
+	ROS_INFO_STREAM("Target pose: " << plan_group.getPoseTarget());
+	
+    // Perform the planning step, and if it succeeds display the current
+    // arm trajectory and move the arm
+    moveit::planning_interface::MoveGroup::Plan goal_plan;
+    if (plan_group.plan(goal_plan))
+    {
+        moveit_msgs::DisplayTrajectory display_msg;
+        display_msg.trajectory_start = goal_plan.start_state_;
+        display_msg.trajectory.push_back(goal_plan.trajectory_);
+        display_pub.publish(display_msg);
+		//ROS_INFO_STREAM("Target pose: " << plan_group.getPoseTarget());
+
+        sleep(5.0);
+
+        plan_group.move();
+    }
+
+    ROS_INFO_STREAM("Current pose: " << plan_group.getCurrentPose());
+    ros::shutdown();
+
+    return 0;
+}
